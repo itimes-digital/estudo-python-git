386d5d40050e666e9eec510df84e234a4651b6cc
Anderson de Sousa <itimes.digital@gmail.com>
2020-02-29 20:14:10 -0300

    Estudo sobre números em python

--- hello_number.py
+++ hello_number.py
@@ -15,3 +15,47 @@ result = 3 ** 3
 print(result)
  
 print(10 ** 6)
+
+#Operacoes
+result = 2 + 3 * 4 #Valor correto eh 14
+print(result)
+
+result = (2 + 3) * 4 #Valor correto eh 20
+print(result)
+
+#Float
+result = 0.2 + 0.1
+print(result)
+
+result = 0.1 + 0.2
+print(result)
+
+result = 0.1 + 0.1
+print(result)
+
+result = 0.2 * 0.1
+print(result)
+
+result = 0.2 * 0.1
+print(result)
+
+result = 2 * 0.2
+print(result)
+
+result = 0.2 * 4
+print(result)
+
+age = 40
+message = "Feliz " + str(age) + " anos de aniversário?" #Apenas para dizer que deve ser tratado como String
+print(message)
+
+result = 3 / 2
+print(result)
+
+result = 10 % 2 #Mode, para saber o resto de uma divisão
+print(result)
+
+print(5 + 3)
+print(2 * 4)
+print(16 - 8)
+print(80 / 10)
