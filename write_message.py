612b3b115aef5fe2e8601b786a41088c625d6368
Anderson de Sousa <itimes.digital@gmail.com>
2020-03-09 16:49:29 -0300

    escrita em arquivo

---
+++ write_message.py
@@ -0,0 +1,21 @@
+"""Realizando leitura de arquivos"""
+filename = 'C:\\Users\\admin\\Documents\\estudos\\text-file\\programming.txt'
+
+"""
+-> 'r' é modo de leitura, a omissão o Python só permite leitura
+-> 'w' é modo de escrita, elimina o que já foi escrito em open apaga o arquivo
+-> 'a' é modo de concatenação
+-> 'r+' é modo de escrita e leitura
+"""
+
+with open(filename, 'w') as file_object:
+	file_object.write("Estou programando em Python...\n")
+	file_object.write("Estou programando em Java...\n")
+	file_object.write("Estou programando em R...\n")
+	file_object.write("Estou estudando data science!\n")
+
+with open(filename) as file_object_open:
+	for line in file_object_open:
+		print(line)
+		
+
