d7e421d6f26b98a5f823b8bed2c367915784ea2f
Anderson de Sousa <aasousa@sompo.com.br>
2020-04-19 12:25:22 -0300

    estudo do algoritmo svm

---
+++ machine-learning/svm/svm_credit_data.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 21:56:15 2020
+
+@author: admin
+"""
+
+import pandas as pd
+
+base = pd.read_csv('D:/estudo-machine-learning/estudo-python/machine-learning/dataset/credit_data.csv')
+
+#ajuste de valores inconsistentes
+base.loc[base.age < 0, 'age'] = 40.92
+
+#extração de colunas 1, 2 e 3 essenciais para a análise
+previsores = base.iloc[:, 1:4].values
+
+#extração de coluna 4 essenciais para a classificação
+classe = base.iloc[:, 4].values
+
+import numpy as np
+from sklearn.impute import SimpleImputer
+
+#Eliminar valores faltantes colocando o valor da média
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')# , axis=0
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+from sklearn.preprocessing import StandardScaler
+
+#Mudar o escalonamento dos dados para calcular espaço euclidiano
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+
+#Gerar uma amostra de treinamento - previsores e classe
+#Gerar uma amostra de teste - previsores e classe
+from sklearn.model_selection import train_test_split
+#Proporção de 25%
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, 
+                                classe, 
+                                test_size=0.25,
+                                random_state=0)
+
+#importação da biblioteca
+from sklearn.svm import SVC
+#criação do classificador
+"""
+rbf = 0,982% | rbf + C 0.50 = 0,976% | rbf + C 2.0 = 0,988%
+linear = 0,946%
+sigmoid = 0,838%
+poly = 0,968%
+"""
+classificador = SVC(kernel='rbf', random_state=1, C = 2.0)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+import collections 
+#Válido para compreender qual o percentual esperado de acuracidade e determinar
+#qual classificador é melhor para tal problema 
+print(collections.Counter(classe_teste))
+
+
